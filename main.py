69920d741b9708e26b2ad15da16d3c8318b237cc
Jeff Kloosterman <kloosterman.jeff@gmail.com>
2014-10-01 19:04:57 -0400

    Make a tile grid and make it scroll

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ import random
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
 from kivy.properties import (NumericProperty,
-                             StringProperty,
-                             ReferenceListProperty,
-                             ObjectProperty)
-from kivy.vector import Vector
-from kivy.clock import Clock
-from kivy.atlas import Atlas
+                             StringProperty)
+
 
 class Tile(Widget):
     grid_x = NumericProperty(0)
@@ -20,7 +19,7 @@ class Tile(Widget):
 
     @staticmethod
     def new_tile(grid_x, grid_y, scale, source):
-        tile = Tile()
+        tile = Tile(size_hint=(None, None))
         tile.scale = scale
         tile.grid_x = grid_x
         tile.grid_y = grid_y
@@ -28,19 +27,30 @@ class Tile(Widget):
         return tile
 
 
-class Grid(Widget):
-    def build_grid(self, nx, ny):
+class Grid(FloatLayout):
+    tiles_scale = NumericProperty()
+    tiles_x = NumericProperty()
+    tiles_y = NumericProperty()
+
+    def build_grid(self, nx, ny, scale):
+        self.tiles_x = nx
+        self.tiles_y = ny
+        self.tiles_scale = scale
         for j in range(ny):
             for i in range(nx):
-                source = 'assets/tuxemon/tuxemon-{j}-{i}.png'.format(j=j, i=i)
-                self.add_widget(Tile.new_tile(i, j, 50, source))
+                y = random.randint(0,12)
+                x = random.randint(0,7)
+                source = 'assets/tuxemon/tuxemon-{}-{}.png'.format(y, x)
+                self.add_widget(Tile.new_tile(i, j, scale, source))
 
 
 class HexApp(App):
     def build(self):
-        game = Grid()
-        game.build_grid(10, 10)
-        return game
+        game = Grid(size_hint=(None, None))
+        game.build_grid(100, 100, 32)
+        root = ScrollView(size_hint=(1, 1))
+        root.add_widget(game)
+        return root
 
 
 if __name__ == '__main__':
