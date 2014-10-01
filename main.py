6a645ffafc9b411e10d2e4e35d53d258c76edb1f
Jeff Kloosterman <kloosterman.jeff@gmail.com>
2014-10-01 00:51:19 -0400

    Render a square tile grid

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 
+import os
 import random
 
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import (NumericProperty,
+                             StringProperty,
                              ReferenceListProperty,
                              ObjectProperty)
 from kivy.vector import Vector
@@ -11,26 +13,33 @@ from kivy.clock import Clock
 from kivy.atlas import Atlas
 
 class Tile(Widget):
-    pass
+    grid_x = NumericProperty(0)
+    grid_y = NumericProperty(0)
+    scale = NumericProperty(25)
+    source = StringProperty()
+
+    @staticmethod
+    def new_tile(grid_x, grid_y, scale, source):
+        tile = Tile()
+        tile.scale = scale
+        tile.grid_x = grid_x
+        tile.grid_y = grid_y
+        tile.source = source
+        return tile
 
 
 class Grid(Widget):
-    def build_grid(self, x, y, atlas):
-        for j in range(y):
-            for i in range(x):
-                tile = Tile()
-                tile.pos = (i * 100, j * 100)
-                key = random.choice(atlas.textures.keys())
-                tile.source = atlas[key]
-                self.add_widget(tile)
-
+    def build_grid(self, nx, ny):
+        for j in range(ny):
+            for i in range(nx):
+                source = 'assets/tuxemon/tuxemon-{j}-{i}.png'.format(j=j, i=i)
+                self.add_widget(Tile.new_tile(i, j, 50, source))
 
 
 class HexApp(App):
     def build(self):
-        self.atlas = Atlas('assets/tuxemon.atlas')
         game = Grid()
-        game.build_grid(4, 5, self.atlas)
+        game.build_grid(10, 10)
         return game
 
 
