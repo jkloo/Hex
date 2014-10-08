91735464c91ca2ce3f0b644a2790a6e38eac79fd
Jeff Kloosterman <kloosterman.jeff@gmail.com>
2014-10-08 18:55:55 -0400

    Decouple movement and animation

--- character.py
+++ character.py
@@ -13,8 +13,10 @@ from kivy.properties import (NumericProperty,
 
 class Character(Widget):
     _animframe = NumericProperty(0)
-    _animating = BooleanProperty(False)
     _animation = {}
+    _animating = BooleanProperty(False)
+    _moveframe = NumericProperty(0)
+    _moving = BooleanProperty(False)
     grid_x = NumericProperty(0)
     grid_y = NumericProperty(0)
     grid_scale = NumericProperty(25)
@@ -30,43 +32,59 @@ class Character(Widget):
         kwargs['size_hint'] = (None, None)
         super(Character, self).__init__(*args, **kwargs)
 
-    def move(self, direction, dt=0.6):
+    def move(self, direction, dt=0.3, movesteps=0):
         capture = False
-        if direction in ['left', 'right', 'up', 'down'] and not self._animating:
+        if direction in ['left', 'right', 'up', 'down'] and not self._animating and not self._moving:
             capture = True
             if self.facing == direction:
                 self._animating = True
-                self._animframe = len(self._animation.get(direction))
-                amount = self.grid_scale / self._animframe
-                dt = dt / self._animframe
-                Clock.schedule_once(partial(self._animate, direction, amount), dt)
+                self._animframe = len(self._animation.get(direction, []))
+                self._moving = True
+                self._moveframe = movesteps or self._animframe
+                amount = self.grid_scale / self._moveframe
+                anim_dt = dt / self._animframe
+                move_dt = dt / self._moveframe
+                Clock.schedule_once(partial(self._move, direction, amount), move_dt)
+                Clock.schedule_once(partial(self._animate, direction), anim_dt)
             else:
                 self.source = self._animation.get(direction)[0]
                 self.facing = direction
         return capture
 
-    def _animate(self, direction, amount, dt):
-        self._animframe -= 1
+    def _move(self, direction, amount, dt):
+        self._moveframe -= 1
         if direction == 'left':
             self.x -= amount
-            if self._animframe == 0:
+            if self._moveframe == 0:
                 self.grid_x -= 1
+                self._moving = False
+
         elif direction == 'right':
             self.x += amount
-            if self._animframe == 0:
+            if self._moveframe == 0:
                 self.grid_x += 1
+                self._moving = False
+
         elif direction == 'up':
             self.y += amount
-            if self._animframe == 0:
+            if self._moveframe == 0:
                 self.grid_y += 1
+                self._moving = False
+
         elif direction == 'down':
             self.y -= amount
-            if self._animframe == 0:
+            if self._moveframe == 0:
                 self.grid_y -= 1
+                self._moving = False
+        if self._moving:
+            Clock.schedule_once(partial(self._move, direction, amount), dt)
+
+    def _animate(self, direction, dt):
+        self._animframe -= 1
         if self._animframe != 0:
             animlist = self._animation.get(direction)
             self.source = animlist[(animlist.index(self.source) + 1) % (len(animlist))]
-            Clock.schedule_once(partial(self._animate, direction, amount), dt)
+            Clock.schedule_once(partial(self._animate, direction), dt)
         else:
             self._animating = False
             self.source = self._animation.get(direction)[0]
